fe6e7fb55bd71bf4478231c70041fa9fe21ab976
Grégoire Jadi <gregoire.jadi@gmail.com>
2014-04-21 17:42:51 +0900

    Dump the Type System

--- src/main/java/jgreg/internship/nii/AE/PubMedXMIWriter.java
+++ src/main/java/jgreg/internship/nii/AE/PubMedXMIWriter.java
@@ -1,7 +1,9 @@
 package jgreg.internship.nii.AE;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import jgreg.internship.nii.types.ID;
 import org.apache.log4j.Logger;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
@@ -9,6 +11,8 @@ import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.fit.util.CasIOUtil;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.util.TypeSystemUtil;
+import org.xml.sax.SAXException;
 
 public class PubMedXMIWriter
         extends org.apache.uima.fit.component.JCasAnnotator_ImplBase {
@@ -18,15 +22,20 @@ public class PubMedXMIWriter
     @ConfigurationParameter(name = OUTPUT_DIRECTORY, mandatory = false, defaultValue="/tmp/xmi/")
     private String outputDirectory;
     
+    private boolean ts_dumped = false;
+    
     @Override
     public void process(JCas jCas) throws AnalysisEngineProcessException {
+        if (!ts_dumped) {
+            writeTs(jCas);
+        }
         String pmid = JCasUtil.selectSingle(jCas, ID.class).getPMID();
         
         // Ensure that the outputDirectory exists
         File dir = new File(outputDirectory);
         dir.mkdirs();
 
-        File outputFile = new File(outputDirectory + pmid + ".xmi");
+        File outputFile = new File(outputDirectory, pmid + ".xmi");
 
         logger.info("Dumping to `" + outputFile.getAbsolutePath() + "'...");
         try {
@@ -34,5 +43,17 @@ public class PubMedXMIWriter
         } catch (IOException ex) {
             logger.fatal("Couldn't dump `" + outputFile + "''", ex);
         }
+    }
+
+    private void writeTs(JCas jcas) throws AnalysisEngineProcessException {
+        try (OutputStream os = new FileOutputStream(
+                new File(outputDirectory, "ts.xml"))) {
+            TypeSystemUtil.typeSystem2TypeSystemDescription(
+                    jcas.getTypeSystem())
+                    .toXML(os);
+        } catch (IOException | SAXException ex) {
+            throw new AnalysisEngineProcessException(ex);
+        }
+        ts_dumped = true;
     }
 }
