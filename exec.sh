b25c4bf3ecc959b1559374f04d2c0d1e6e3a919b
Grégoire Jadi <gregoire.jadi@gmail.com>
2014-04-24 15:09:03 +0900

    exec.sh: Optional cleanup

--- exec.sh
+++ exec.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
-JAVA_HOME=~/bin/java8/ mvn clean package
+[[ $1 = "clean" ]] && JAVA_HOME=~/bin/java8/ mvn clean package
 ~/bin/java8/bin/java -cp target/csa-1.0-SNAPSHOT-jar-with-dependencies.jar jgreg.internship.nii.WF.Pipeline
