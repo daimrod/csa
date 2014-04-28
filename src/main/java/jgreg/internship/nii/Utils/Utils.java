2465a24b7f35f8485c33ac5b91c6a3eb1e8eaa86
Grégoire Jadi <gregoire.jadi@gmail.com>
2014-04-29 02:24:40 +0900

    deleted Utils

--- src/main/java/jgreg/internship/nii/Utils/Utils.java
+++ src/main/java/jgreg/internship/nii/Utils/Utils.java
@@ -5,19 +5,5 @@ import java.util.regex.Pattern;
 
 public class Utils {
 
-    public static Pattern PatternFactory(List<String> list) {
-        StringBuilder acc = new StringBuilder();
-        if (list.size() > 0) {
-            String last = list.remove(list.size() - 1);
-            
-            for (String s : list) {
-                acc.append(s)
-                        .append('|');
-            }
-            
-            acc.append(last);
-        }
-
-        return Pattern.compile(acc.toString());
-    }
+    
 }
