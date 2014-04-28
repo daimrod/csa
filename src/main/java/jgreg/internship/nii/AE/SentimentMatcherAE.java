36b82ac99bca7c5e4a9009d4a147f942fec66e1c
Grégoire Jadi <gregoire.jadi@gmail.com>
2014-04-28 16:51:36 +0900

    Add a new class to store sentiment from context and use it

--- src/main/java/jgreg/internship/nii/AE/SentimentMatcherAE.java
+++ src/main/java/jgreg/internship/nii/AE/SentimentMatcherAE.java
@@ -6,10 +6,13 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import jgreg.internship.nii.RES.MatcherRES;
+import jgreg.internship.nii.RES.SentimentAttr;
 import jgreg.internship.nii.Utils.Utils;
 import jgreg.internship.nii.types.CitationContext;
 import jgreg.internship.nii.types.Sentence;
+
 import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 import org.apache.uima.UimaContext;
@@ -20,35 +23,50 @@ import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
 
-public class MatcherAE extends org.apache.uima.fit.component.JCasAnnotator_ImplBase {
-    private static final Logger logger = Logger.getLogger(MatcherAE.class.getCanonicalName());
+public class SentimentMatcherAE extends org.apache.uima.fit.component.JCasAnnotator_ImplBase {
+    private static final Logger logger = Logger.getLogger(SentimentMatcherAE.class.getCanonicalName());
 
-    final static String MATCHER_KEY = "matcher";
+	/**
+	 * The resource that contains the sentiment scores.
+	 */
+	final static String MATCHER_KEY = "matcher";
     @ExternalResource(key = MATCHER_KEY)
-    private MatcherRES matcherRES;
+    private MatcherRES<SentimentAttr> matcherRES;
 
+	/**
+	 * The name of the file in which we will look for patterns.
+	 */
     final static String PARAM_INPUT_MATCH = "input";
     @ConfigurationParameter(name = PARAM_INPUT_MATCH, mandatory = true)
     private File inputFile;
 
+	/**
+	 * The name of the class of sentiment that we are matching.
+.	 */
     final static String PARAM_MATCHER_NAME = "name";
     @ConfigurationParameter(name = PARAM_MATCHER_NAME, mandatory = true)
     String name;
     
+	/**
+	 * FIXME The pattern that will be used to determine whether the
+	 * given context should be recorded.
+	 */
     private Pattern pattern;
     
-
     @Override
     public void initialize(UimaContext context) throws ResourceInitializationException {
-        matcherRES = (MatcherRES) context.getConfigParameterValue(MATCHER_KEY);
+        // Retrieve the parameters from the context
+        matcherRES = (MatcherRES<SentimentAttr>) context.getConfigParameterValue(MATCHER_KEY);
         inputFile = new File((String) context.getConfigParameterValue(PARAM_INPUT_MATCH));
         name = (String) context.getConfigParameterValue(PARAM_MATCHER_NAME);
 
+        // Be sure the patterns file exists
         if (!inputFile.exists()) {
             logger.fatal("input file `" + inputFile.getAbsolutePath() + "' does not exist");
             throw new ResourceInitializationException();
         }
-        
+
+        // Read the patterns...
         try {
             pattern = Utils.PatternFactory(FileUtils.readLines(inputFile));
         } catch (IOException ex) {
@@ -59,12 +77,11 @@ public class MatcherAE extends org.apache.uima.fit.component.JCasAnnotator_ImplB
     
     @Override
     public void process(JCas jCas) throws AnalysisEngineProcessException {
-        Map<CitationContext, Collection<Sentence>> map = JCasUtil.indexCovered(jCas, CitationContext.class, Sentence.class);
         for (CitationContext context : JCasUtil.select(jCas, CitationContext.class)) {
             for (Sentence sent : map.get(context)) {
                 Matcher match = pattern.matcher(sent.getCoveredText());
                 while (match.find()) {
-                    matcherRES.add(context.getPMID(), match.group());
+                    matcherRES.add(context.getPMID(), new SentimentAttr(name, SentimentAttr.DEFAULT_SCORE, match.group()));
                 }
             }
         }
--- src/main/java/jgreg/internship/nii/RES/MatcherRES.java
+++ src/main/java/jgreg/internship/nii/RES/MatcherRES.java
@@ -4,30 +4,31 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+
 import org.apache.log4j.Logger;
 import org.apache.uima.resource.DataResource;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.resource.SharedResourceObject;
 
-public final class MatcherRES implements SharedResourceObject {
+public final class MatcherRES<T> implements SharedResourceObject {
     private static final Logger logger = Logger.getLogger(MatcherRES.class.getCanonicalName());
     
-    private Map<String, List<String>> matches;
+    private Map<String, List<T>> matches;
     
     @Override
     public void load(DataResource aData) throws ResourceInitializationException {
         matches = new HashMap<>();
     }
 
-    synchronized public void add(String key, String val) {
+    synchronized public void add(String key, T val) {
         if (matches.containsKey(key)) {
             matches.get(key).add(val);
         } else {
-            List<String> list = new LinkedList<>();
+            List<T> list = new LinkedList<>();
             list.add(val);
             matches.put(key, list);
         }
     }
 
-    public Map<String, List<String>> getMatches() { return matches; }
+    public Map<String, List<T>> getMatches() { return matches; }
 }
