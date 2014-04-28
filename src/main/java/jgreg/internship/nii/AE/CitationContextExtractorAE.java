1876e264c3f2b248601729815f65033aef5b33cf
Grégoire Jadi <gregoire.jadi@gmail.com>
2014-04-28 19:16:16 +0900

    Only one CitationContext per sentence

--- src/main/java/jgreg/internship/nii/AE/CitationContextExtractorAE.java
+++ src/main/java/jgreg/internship/nii/AE/CitationContextExtractorAE.java
@@ -1,18 +1,22 @@
 package jgreg.internship.nii.AE;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import jgreg.internship.nii.types.Citation;
 import jgreg.internship.nii.types.CitationContext;
 import jgreg.internship.nii.types.Sentence;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.log4j.Logger;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
 import org.apache.uima.fit.descriptor.ConfigurationParameter;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.cas.StringList;
 
 public class CitationContextExtractorAE extends org.apache.uima.fit.component.JCasAnnotator_ImplBase {
     private static final Logger logger = Logger.getLogger(CitationContextExtractorAE.class.getCanonicalName());
@@ -21,33 +25,43 @@ public class CitationContextExtractorAE extends org.apache.uima.fit.component.JC
     @ConfigurationParameter(name = PARAM_WINDOW_SIZE, mandatory = true)
     private Integer windowSize;
 
+    private Set<Sentence> cache = new HashSet<>();
+
     @Override
     public void process(JCas jCas) throws AnalysisEngineProcessException {
         Map<Citation, Collection<Sentence>> map = JCasUtil.indexCovering(jCas, Citation.class, Sentence.class);
 
         for (Citation citation : JCasUtil.select(jCas, Citation.class)) {
-            CitationContext context = new CitationContext(jCas);
-            context.setPMID(citation.getPMID());
+            Sentence sent = map.get(citation).iterator().next();
 
+            // No need to duplicate the CitationContext
+            if (cache.contains(sent)) {
+                continue;
+            } else {
+                cache.add(sent);
+            }
+
+            int begin, end;
             // Before
             List<Sentence> precedings = JCasUtil.selectPreceding(Sentence.class, citation, windowSize);
             if (precedings.size() > 0) {
-                context.setBegin(precedings.get(0).getBegin());
+                begin = precedings.get(0).getBegin();
             } else {
-                Sentence sent = map.get(citation).iterator().next();
-                context.setBegin(sent.getBegin());
+                begin = sent.getBegin();
             }
 
             // After
             List<Sentence> followings = JCasUtil.selectFollowing(Sentence.class, citation, windowSize);
             if (followings.size() > 0) {
-                context.setEnd(followings.get(followings.size() - 1).getEnd());
+                end = followings.get(followings.size() - 1).getEnd();
             } else {
-                Sentence sent = map.get(citation).iterator().next();
-                context.setEnd(sent.getEnd());
+                end = sent.getEnd();
             }
             
-            context.addToIndexes();
+            CitationContext context = new CitationContext(jCas);
+            context.setBegin(begin);
+            context.setEnd(end);
+            context.addToIndexes(jCas);
         }
     }
 }
