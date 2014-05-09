cc1c5205c2577350170fae9a27d275bf439dd105
Grégoire Jadi <gregoire.jadi@gmail.com>
2014-05-09 17:55:13 +0900

    * src/main/java/jgreg/internship/nii/CR/PubMedReaderCR.java: Fix docIndex and use the new Resource

--- src/main/java/jgreg/internship/nii/CR/PubMedReaderCR.java
+++ src/main/java/jgreg/internship/nii/CR/PubMedReaderCR.java
@@ -12,6 +12,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import jgreg.internship.nii.RES.StringListRES;
 import jgreg.internship.nii.types.Filename;
 
 import org.apache.commons.io.FileUtils;
@@ -21,6 +22,7 @@ import org.apache.uima.collection.CollectionException;
 import org.apache.uima.fit.component.JCasCollectionReader_ImplBase;
 import org.apache.uima.fit.component.ViewCreatorAnnotator;
 import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.fit.descriptor.ExternalResource;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.util.Progress;
@@ -31,89 +33,86 @@ import org.apache.uima.util.Progress;
  */
 public class PubMedReaderCR extends JCasCollectionReader_ImplBase {
 
-    private static final Logger logger = Logger.getLogger(PubMedReaderCR.class.getCanonicalName());
+	private static final Logger logger = Logger.getLogger(PubMedReaderCR.class
+			.getCanonicalName());
 
-    /**
-     * Path to the PubMed Corpus
-     */
-    public static final String INPUT_DIRECTORY = "inputDirectoryName";
-    @ConfigurationParameter(name = INPUT_DIRECTORY, mandatory = true)
-    private String inputDirectoryName;
-    private File inputDirectory;
+	/**
+	 * Path to the PubMed Corpus
+	 */
+	public static final String INPUT_DIRECTORY = "inputDirectoryName";
+	@ConfigurationParameter(name = INPUT_DIRECTORY, mandatory = true)
+	private String inputDirectoryName;
+	private File inputDirectory;
 
+	public static final String CORPUS_ARTICLES = "corpusArticles";
+	@ExternalResource(key = CORPUS_ARTICLES, mandatory = true)
+	StringListRES corpusArticles;
 
-    public static final String INPUT_LIST = "inputFileNameList";
-    @ConfigurationParameter(name = INPUT_LIST, mandatory = true)
-    private String inputFileNameList;
-    private File inputFileList;
+	private List<File> files;
+	private Iterator<File> filesIt;
+	private int docIndex = 0;
 
-    private List<File> files;
-    private Iterator<File> filesIt;
-    private int docIndex = 0;
+	/**
+	 * Get PubMedReaderCR ready to read files in INPUT_DIRECTORY.
+	 *
+	 * @param context
+	 * @throws ResourceInitializationException
+	 */
+	@Override
+	public void initialize(UimaContext context)
+			throws ResourceInitializationException {
+		super.initialize(context);
 
-    /**
-     * Get PubMedReaderCR ready to read files in INPUT_DIRECTORY.
-     *
-     * @param context
-     * @throws ResourceInitializationException
-     */
-    @Override
-    public void initialize(UimaContext context) throws ResourceInitializationException {
-        super.initialize(context);
-        
-        logger.info("Listing `" + inputDirectoryName + "'...");
-        inputDirectory = new File(inputDirectoryName);
+		logger.info("Listing `" + inputDirectoryName + "'...");
+		inputDirectory = new File(inputDirectoryName);
 
-        if (!inputDirectory.exists()) {
-            logger.error("could not find the PubMed directory at `" + inputDirectoryName + "'");
-            throw new ResourceInitializationException();
-        }
+		if (!inputDirectory.exists()) {
+			logger.error("could not find the PubMed directory at `"
+					+ inputDirectoryName + "'");
+			throw new ResourceInitializationException();
+		}
 
-        inputFileList = new File(inputFileNameList);
-        if (!inputFileList.exists()) {
-            logger.error("could not find the input file at `" + inputFileNameList + "'");
-            throw new ResourceInitializationException();
-        }
+		files = corpusArticles.getList().stream()
+				.map(filename -> new File(inputDirectory, filename))
+				.filter(file -> {
+					if (file.exists()) {
+						return true;
+					} else {
+						logger.warn(file.getAbsolutePath() + " doesn't exist");
+						return false;
+					}
+				}).collect(Collectors.toList());
+		filesIt = files.iterator();
+	}
 
-        try {
-            files = FileUtils.readLines(inputFileList).stream()
-                .map(filename -> filename.trim())
-                .filter(filename -> !filename.isEmpty()) // ignore empty lines
-                .filter(filename -> !filename.startsWith("#")) // lines starting with a '#' are ignored
-                .map(filename -> new File(inputDirectory, filename))
-                .filter(file -> { if (file.exists()) { return true; } else { logger.warn(file.getAbsolutePath() + " doesn't exist"); return false; }})
-                .collect(Collectors.toList());
-            filesIt = files.iterator();
-        } catch (IOException ex) {
-            throw new ResourceInitializationException(ex);
-        }
-    }
-
-    @Override
-    public void getNext(JCas jCas) throws IOException, CollectionException, FileNotFoundException {
-        File file = filesIt.next();
-        logger.info("Reading[" + docIndex + "/" + files.size() + " ] `" + file.getAbsolutePath() + "'...");
-        try {
-            JCas originalText = ViewCreatorAnnotator.createViewSafely(jCas, "originalText");
-            originalText.setDocumentText(FileUtils.readFileToString(file));
-            Filename filename = new Filename(originalText);
-            filename.setFilename(file.getAbsolutePath());
-            filename.addToIndexes();
-        } catch (Exception ex) {
-            throw new CollectionException(ex);
-        }
-
+	@Override
+	public void getNext(JCas jCas) throws IOException, CollectionException,
+			FileNotFoundException {
         docIndex++;
-    }
 
-    @Override
-    public boolean hasNext() throws IOException, CollectionException {
-        return filesIt.hasNext();
-    }
+		File file = filesIt.next();
+		logger.info("Reading[" + docIndex + "/" + files.size() + " ] `"
+				+ file.getAbsolutePath() + "'...");
+		try {
+			JCas originalText = ViewCreatorAnnotator.createViewSafely(jCas,
+					"originalText");
+			originalText.setDocumentText(FileUtils.readFileToString(file));
+			Filename filename = new Filename(originalText);
+			filename.setFilename(file.getAbsolutePath());
+			filename.addToIndexes();
+		} catch (Exception ex) {
+			throw new CollectionException(ex);
+		}
+	}
 
-    @Override
-    public Progress[] getProgress() {
-        return new Progress[docIndex];
-    }
+	@Override
+	public boolean hasNext() throws IOException, CollectionException {
+		return filesIt.hasNext();
+	}
+
+	@Override
+	public Progress[] getProgress() {
+		return new Progress[docIndex];
+	}
 
 }
