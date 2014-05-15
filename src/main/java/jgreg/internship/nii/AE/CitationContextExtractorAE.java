3debe98ef28cb889ddf298b7caf0a02ac8b8bfba
Grégoire Jadi <gregoire.jadi@gmail.com>
2014-05-15 16:05:41 +0900

    * src/main/java/jgreg/internship/nii/AE/CitationContextExtractorAE.java: Add comment and javadoc

--- src/main/java/jgreg/internship/nii/AE/CitationContextExtractorAE.java
+++ src/main/java/jgreg/internship/nii/AE/CitationContextExtractorAE.java
@@ -21,39 +21,62 @@ import org.apache.uima.jcas.JCas;
 import org.apache.uima.jcas.cas.FSArray;
 
 /**
- * Add CitationContext annotation around all Citation. The size of the context
- * correspond to the number of Sentences indicated by PARAM_WINDOW_SIZE.
+ * Add CitationContext annotation around all Citation.
  *
- * For example given the following scheme: s1. s2. s3. s4. s5. s6.
+ * The size of the context corresponds to the number of
+ * {@link jgreg.internship.nii.types.Sentence} indicated by
+ * {@link #PARAM_WINDOW_SIZE}.
  *
- * Let's suppose there is a citation in s2 and PARAM_WINDOW_SIZE is set to 2.
- * Then there will be a CitationContext covering s1, s2, s3, and s4. That is, at
- * most PARAM_WINDOW_SIZE sentence before and after the sentence.
+ * For example given the following sentences: {@code s1. s2. s3. s4. s5. s6}.
  *
- * No matter what, the sentence in which the citation occurs is always covered.
+ * Let's suppose there is a {@link jgreg.internship.nii.types.Citation} in s2
+ * and {@link #PARAM_WINDOW_SIZE} is set to 2. Then there will be a
+ * {@link jgreg.internship.nii.types.CitationContext} covering s1, s2, s3, and
+ * s4. That is, at most {@link #PARAM_WINDOW_SIZE} sentence before and after the
+ * sentence.
+ *
+ * No matter what, the {@link jgreg.internship.nii.types.Sentence} in which the
+ * {@link jgreg.internship.nii.types.Citation} occurs is always covered by the
+ * {@link jgreg.internship.nii.types.CitationContext}.
  *
  * @author Grégoire Jadi
  */
 public class CitationContextExtractorAE extends
 		org.apache.uima.fit.component.JCasAnnotator_ImplBase {
+
+	/** The Constant logger. */
 	private static final Logger logger = Logger
 			.getLogger(CitationContextExtractorAE.class.getCanonicalName());
 
 	/**
-	 * The size of the window to consider around the Sentence in which Citations
-	 * occur.
+	 * The size of the window to consider around the
+	 * {@link jgreg.internship.nii.types.Sentence} in which
+	 * {@link jgreg.internship.nii.types.Citation} occur.
 	 */
 	public static final String PARAM_WINDOW_SIZE = "windowSize";
 	@ConfigurationParameter(name = PARAM_WINDOW_SIZE, mandatory = true)
 	private Integer windowSize;
 
 	/**
-	 * List of PMIDS to focus on (i.e. ignore all the others)
+	 * List of PMIDS to focus on.
+	 *
+	 * For example, an article may cite different articles, but we might not be
+	 * interested by all {@link jgreg.internship.nii.types.Citation}.
+	 *
+	 * With this resource, we can choose to focus on some
+	 * {@link jgreg.internship.nii.types.Citation} and ignore all the others.
 	 */
 	public static final String FOCUSED_ARTICLES = "focusedArticles";
 	@ExternalResource(key = FOCUSED_ARTICLES, mandatory = false)
 	StringListRES focusedArticles;
 
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see
+	 * org.apache.uima.analysis_component.JCasAnnotator_ImplBase#process(org
+	 * .apache.uima.jcas.JCas)
+	 */
 	@Override
 	public void process(JCas jCas) throws AnalysisEngineProcessException {
 		Map<Sentence, Collection<Citation>> sentence2Citations = JCasUtil
@@ -62,22 +85,32 @@ public class CitationContextExtractorAE extends
 		for (Sentence sentence : JCasUtil.select(jCas, Sentence.class)) {
 			List<Citation> citations;
 			if (focusedArticles == null) {
+                // If we don't have a list of articles to focus on, we
+                // look at all of them.
 				citations = new ArrayList(sentence2Citations.get(sentence));
 			} else {
-				citations = sentence2Citations.get(sentence).stream()
-						.filter(citation -> focusedArticles.contains(citation.getPMID()))
-						.collect(Collectors.toList());
+                // Otherwise, we only consider the Citation that
+                // interest us.
+				citations = sentence2Citations
+						.get(sentence)
+						.stream()
+						.filter(citation -> focusedArticles.contains(citation
+								.getPMID())).collect(Collectors.toList());
 			}
 
-            // Skip Sentence with no Citations (to focus on) in it.
-            if (citations.isEmpty()) {
-                continue;
-            }
+			// Skip Sentence with no Citation (to focus on) in it.
+			if (citations.isEmpty()) {
+				continue;
+			}
 
+            // Convert the List<Citation> to List<FeatureStructure>
+            // because that's what UIMA manipulates.
 			List<FeatureStructure> citationsFS = new ArrayList(citations);
 
 			int begin, end;
-			// Before
+			// Extract the Sentence that belongs to the
+			// CitationContext *before* the Sentence in which the
+			// Citation occurs.
 			List<Sentence> precedings = JCasUtil.selectPreceding(
 					Sentence.class, sentence, windowSize);
 			if (precedings.size() > 0) {
@@ -86,7 +119,9 @@ public class CitationContextExtractorAE extends
 				begin = sentence.getBegin();
 			}
 
-			// After
+            // Extract the Sentence that belongs to the
+			// CitationContext *after* the Sentence in which the
+			// Citation occurs.
 			List<Sentence> followings = JCasUtil.selectFollowing(
 					Sentence.class, sentence, windowSize);
 			if (followings.size() > 0) {
@@ -99,6 +134,8 @@ public class CitationContextExtractorAE extends
 			context.setBegin(begin);
 			context.setEnd(end);
 
+            // Unfortunately, UIMA "complex" structures are very
+            // crudes. That's why this code is ugly.
 			FSArray fsArray = new FSArray(jCas, citationsFS.size());
 			fsArray.copyFromArray(citationsFS
 					.toArray(new FeatureStructure[citationsFS.size()]), 0, 0,
