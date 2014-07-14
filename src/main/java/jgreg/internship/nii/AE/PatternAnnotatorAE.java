e5eac08937fe6263ad3b1ce02bf4e9a59af0d5f1
Grégoire Jadi <gregoire.jadi@gmail.com>
2014-07-14 11:55:28 +0900

    AE/PatternAnnotatorAE.java: Ignore the CASE when matching patterns.

--- src/main/java/jgreg/internship/nii/AE/PatternAnnotatorAE.java
+++ src/main/java/jgreg/internship/nii/AE/PatternAnnotatorAE.java
@@ -1,20 +1,20 @@
-// 
+//
 // Author:: Grégoire Jadi <daimrod@gmail.com>
 // Copyright:: Copyright (c) 2014, Grégoire Jadi
 // All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
-// 
+//
 //    1. Redistributions of source code must retain the above copyright
 //       notice, this list of conditions and the following disclaimer.
-// 
+//
 //    2. Redistributions in binary form must reproduce the above
 //       copyright notice, this list of conditions and the following
 //       disclaimer in the documentation and/or other materials provided
 //       with the distribution.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY GRÉGOIRE JADI ``AS IS'' AND ANY
 // EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 // IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
@@ -27,12 +27,12 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
 // OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 // SUCH DAMAGE.
-// 
+//
 // The views and conclusions contained in the software and
 // documentation are those of the authors and should not be
 // interpreted as representing official policies, either expressed or
 // implied, of Grégoire Jadi.
-// 
+//
 
 package jgreg.internship.nii.AE;
 
@@ -43,6 +43,7 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 import jgreg.internship.nii.RES.MappingRES;
 import jgreg.internship.nii.Utils.Utils;
@@ -60,6 +61,7 @@ import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
 
 import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.tokensregex.Env;
 import edu.stanford.nlp.ling.tokensregex.TokenSequenceMatcher;
 import edu.stanford.nlp.ling.tokensregex.TokenSequencePattern;
 
@@ -69,12 +71,12 @@ import edu.stanford.nlp.ling.tokensregex.TokenSequencePattern;
  *
  * @author Grégoire Jadi
  */
-public class SentimentFinderAE extends
+public class PatternAnnotatorAE extends
 		org.apache.uima.fit.component.JCasAnnotator_ImplBase {
 
 	/** The Constant logger. */
 	private static final Logger logger = Logger
-			.getLogger(SentimentFinderAE.class.getCanonicalName());
+			.getLogger(PatternAnnotatorAE.class.getCanonicalName());
 
 	/** Default score for sentiment annotation. */
 	private Integer DEFAULT_SCORE = 1;
@@ -104,25 +106,31 @@ public class SentimentFinderAE extends
 		super.initialize(context);
 
 		// Initialize the patterns for all classes
-		patterns = new HashMap<>();
-		try {
-            // Combine all patterns in one big pattern using the
-            // conjunction operator.
+        patterns = new HashMap<>();
+
+        Env env = TokenSequencePattern.getNewEnv();
+        env.setDefaultStringMatchFlags(Pattern.CASE_INSENSITIVE);
+
+        try {
+			// Combine all patterns in one big pattern using the
+			// conjunction operator.
 			for (String className : mapping.keySet()) {
-                if ("order".equals(className)) {
-                    continue;
-                }
-                
+				if ("order".equals(className)) {
+					continue;
+				}
+
 				List<String> strs = new LinkedList<>();
 				for (String filename : mapping.get(className)) {
-                    logger.info("Reading `" + filename + "'...");
+					logger.info("Reading `" + filename + "'...");
 					strs.add(StringUtils.join(
 							Utils.readLines(new File(filename)).stream()
 									.map(line -> "(" + line + ")").iterator(),
 							"|"));
-				}
-				patterns.put(className, TokenSequencePattern
-						.compile(StringUtils.join(strs, "|")));
+                }
+                patterns.put(
+						className,
+						TokenSequencePattern.compile(env,
+								StringUtils.join(strs, "|")));
 			}
 		} catch (IOException ex) {
 			throw new ResourceInitializationException(ex);
